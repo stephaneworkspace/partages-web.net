4cfc95e793dc90284b36057c01bd7338470e2118
stephaneworkspace <s.bressani@bluewin.ch>
2019-12-11 02:24:13 +0100

    Some try with frontend

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#PATH_RUST=/home/stephane/Code/Rust/partages_web_net/
+#cd $PATH_RUST
+#rm -rf public
+#mkdir public
+#cd frontend/
+#npm run build
+#cp -a $PATH_RUST/frontend/dist/* $PATH_RUST/public/
+cargo run web
